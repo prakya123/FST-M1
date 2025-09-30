eac1ae53ccfa31f66b3af9a9117f3a84a1b2def0
prakya123 <90836500+prakya123@users.noreply.github.com>
2025-09-30 23:19:23 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,20 @@
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+     // Create a Car object
+     Car myCar = new Car();
+
+     // Initialize values
+     myCar.make = 2014;
+     myCar.color = "Black";
+     myCar.transmission = "Manual";
+
+     // Call methods
+     myCar.displayCharacteristics();
+     myCar.accelerate();
+     myCar.brake();
+ }
+}
